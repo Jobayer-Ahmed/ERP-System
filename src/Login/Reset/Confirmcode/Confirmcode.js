a1c3cec9b38777a31e24479f6788b7d94ce540e0
Jobayer-Ahmed <jobayeramickey@gmail.com>
2018-05-11 00:31:10 +0600

    Add confirmation code option

--- src/Login/Reset/Confirmcode/Confirmcode.js
+++ src/Login/Reset/Confirmcode/Confirmcode.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Confirmcode from './Confirmcode/Confirmcode'
-import './Reset.css';
+import Newpass from './Newpass/Newpass'
 const reset = require('./reset.png')
 
 class Reset extends Component {
@@ -10,14 +9,6 @@ class Reset extends Component {
 		ref.state = {
 			newpassword: false,
 		}
-		ref.back = ref.back.bind(ref)
-	}
-
-	back() {
-		const ref = this;
-		ref.setState({
-			newpassword: !ref.state.newpassword,
-		})
 	}
 
 	render() {
@@ -35,19 +26,19 @@ class Reset extends Component {
 							<img src={reset} alt="Login Image"/>
 						</div>
 						<div className="text">
-							<b>Reset password</b> <br/>
+							<b>Enter confirmation code</b> <br/>
 							<div>Enter your email address in the form bellow and we will send <br/> you further instructions on how to reset your password.</div>
 						</div>
 						<div className="inputs">
-							<input type="text" placeholder="Email address"/>
+							<input type="text" placeholder="Confirmation code"/>
 						</div>
 						<div className="submit text-center no-gutter">
-							<button className="big" onClick={() => ref.setState({newpassword: !ref.state.newpassword})}>Send activation code</button>
+							<button className="big" onClick={ref.props.back}>Varify code</button>
 						</div>
 					</div>
 				</div>
 				<div className={ref.state.newpassword ? "change" : "change hide" }>
-					<Confirmcode back={ref.back}/>
+					<Newpass />
 				</div>
 			</div>
 		);
